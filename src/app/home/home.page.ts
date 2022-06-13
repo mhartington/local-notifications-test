338960122e133990708655c9712d3c1b40ec8dab
Mike Hartington <mikehartington@gmail.com>
2022-06-13 09:22:19 -0400

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import { LocalNotifications } from '@capacitor/local-notifications';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
-
+  async ngOnInit() {
+    await LocalNotifications.requestPermissions();
+  }
+  async setup() {
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          id: Math.floor(Math.random() * 90),
+          title: 'Testing',
+          body: 'some stuff',
+          schedule: { at: new Date(Date.now() + 1000 * 90) },
+        },
+      ],
+    });
+  }
 }
